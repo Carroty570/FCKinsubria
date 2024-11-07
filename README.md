264ca0318170261f12d00f4c8a1d149ab03f66a9
Carroty570 <mufaleirosit@gmail.com>
2024-11-07 11:32:32 +0100

    Primo commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FCKinsubria
-Repository with all java projects in Insubria
+Repository with all java projects in Insubria]
+
+- Teste.java, testing file for github
---
+++ teste.java
@@ -0,0 +1,10 @@
+
+public class teste {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Ciao coglione");
+
+        
+    }
+}
