13836c7b7f7d7c9377abf5bf693babd1ae0bd766
Carroty570 <mufaleirosit@gmail.com>
2024-11-11 21:44:35 +0100

    ex bank

--- LEZIONI/Murilo/CursoDIO/Bank/AccountBank.java
+++ LEZIONI/Murilo/CursoDIO/Bank/AccountBank.java
@@ -5,11 +5,30 @@ public class AccountBank {
 
         public static void main(String[] args) {
             
-            Scanner sc = new Scanner(System.in);
+            
+            Scanner s = new Scanner(System.in);
 
             System.out.print("What is your name ?> ");
-            String n = sc.next();
-            System.out.println("Seu nome é " + n);
+            String name = s.next();
+
+            System.out.print("Name of your agency ?> ");
+            String agency = s.next();
+
+            System.out.print("What is the number ?> ");
+            int number = s.nextInt();
+
+            System.out.print("Your balance ?> ");
+            double balance = s.nextDouble();
+
+            System.out.print("Hi, " + name + "thanks to chose our bank, your agency is: " + agency + "the number of your account is " + number + "and you have " + balance + "to withdraw");
+
+            
+
+
+
+
+           
+
 
 
         }
