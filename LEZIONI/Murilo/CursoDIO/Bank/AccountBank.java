3c69d9110c69b31ed3d56cb7e390298d4eb2135d
Carroty570 <mufaleirosit@gmail.com>
2024-11-11 21:30:25 +0100

    Alo

---
+++ LEZIONI/Murilo/CursoDIO/Bank/AccountBank.java
@@ -0,0 +1,21 @@
+package Murilo.CursoDIO.Bank;
+import java.util.Scanner;
+
+public class AccountBank {
+
+        public static void main(String[] args) {
+            
+            Scanner sc = new Scanner(System.in);
+
+            System.out.print("What is your name ?> ");
+            String n = sc.next();
+            System.out.println("Seu nome é " + n);
+
+
+        }
+
+    
+}
+
+
+
