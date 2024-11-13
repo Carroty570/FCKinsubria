8263b88fd2d25b24bd4c97b475800a4281375958
Carroty570 <mufaleirosit@gmail.com>
2024-11-13 10:38:52 +0100

    Try catch exeption - Murilo

---
+++ LEZIONI/Murilo/CepValidation/ExException.java
@@ -0,0 +1,29 @@
+package Murilo.CepValidation;
+
+import java.text.NumberFormat;
+import java.text.ParseException;
+
+public class ExException {
+    
+    
+    public static void main(String[] args) {
+        Number valor = Double.valueOf("a1.75");
+
+        try {
+
+            valor = NumberFormat.getInstance().parse("a1.75");
+
+        } catch (ParseException e) {
+
+            
+
+            e.printStackTrace();
+
+        }
+        
+        System.out.println(valor);
+       
+    }
+}
+
+
