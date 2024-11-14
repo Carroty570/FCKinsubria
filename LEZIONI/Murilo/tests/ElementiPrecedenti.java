226f8f279387f733dcdc1130f2ab9f5e2e853082
Carroty570 <mufaleirosit@gmail.com>
2024-11-14 09:58:44 +0100

    Linkedlist - Murilo

---
+++ LEZIONI/Murilo/tests/ElementiPrecedenti.java
@@ -0,0 +1,16 @@
+package Murilo.tests;
+
+public class  ElementiPrecedenti {
+    
+    public boolean[] sommaElementiPrecedentiPari(int[] a) {
+        boolean[] ris = new boolean[a.length];
+        int somma = 0;
+        for(int i =0; i<a.length; i++) {
+        somma += a[i];
+        ris[i] = (somma % 2) == 0? true:false;
+        }
+        return ris;
+        }
+
+
+}
